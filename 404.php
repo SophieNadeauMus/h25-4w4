6475aff6c8e8f1aac8f48e3b9f2713f6f0e75f5d
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-03-24 15:20:50 -0400

    s9c1 - Mise en page de 404.php complété

--- 404.php
+++ 404.php
@@ -3,17 +3,17 @@
   $coord_description = get_theme_mod('coord_description', '');
  ?>
 <?php get_header(); ?>
-<section class="erreur global">
+<section class="erreur">
   <div class="erreur__contenu">
     <h1>Erreur 404</h1>
     <h5>La page que vous cherchez n'existe pas.</h5>
-  </div>
-  <div class="erreur__nav">
+    <div class="erreur__nav">
     <p>Voici quelques liens utiles pour vous aider: </p>
     <?php wp_nav_menu(array(
       'menu' => 'erreur',
       'container' => false,
     )); ?>
+  </div>
   </div>
   <div class="erreur__description">
     <h6>À propos de notre site</h6>
