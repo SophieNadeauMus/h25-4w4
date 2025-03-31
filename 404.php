934063bfafe73e62bbd3006a20d8c182e45808b4
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-03-31 14:52:38 -0400

    s10c1 - Ajout du bouton de retour à l'accueil

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@
   <div class="erreur__contenu">
     <h1 class="erreur__titre"><?= $titre_404; ?></h1>
     <p class="erreur__message"><?= $message_404 ?></p>
-    <button class="erreur__btn" type="submit" style="background-color: <?= $couleur_404; ?>">Retour à l'accueil</button>
+    <a class="erreur__btn" href="<?php echo home_url(); ?>" style="background-color: <?= $couleur_404; ?>">Retour à l'accueil</a>
     <div class="erreur__nav">
     <?php wp_nav_menu(array(
       'menu' => 'erreur',
