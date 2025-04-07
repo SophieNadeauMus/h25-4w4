a82df281613aba64f1d0cf345ede33080b63c112
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-04-07 15:16:01 -0400

    s11c1 - ajout de la classe active pour mettre de l'avant la catégorie sélectionnée

--- js/destination.js
+++ js/destination.js
@@ -2,21 +2,22 @@
 * Script js permettant d'extraire les destinations de voyage
 */
 (function(){
-    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
     const domaine = window.location.href;
-    const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
-
     parcourir_boutons();
 
     // Fonction pour parcourir les boutons de catégorie
     function parcourir_boutons() {
-        const categorie__ul__li = document.querySelectorAll('.categorie__ul__li')
+        const categorie__ul__li = document.querySelectorAll('.categorie__ul__li');
        
         categorie__ul__li.forEach(elm => {
             elm.addEventListener('click', function() {
+                // Supprimer la classe active de tous les boutons
+                categorie__ul__li.forEach(elm => elm.classList.remove('active'));
+                
+                elm.classList.add('active'); // Ajouter la classe active au bouton cliqué
+                
                 // console.log(elm.dataset.category_id);
-
-                const categoryId = this.dataset.category_id;
+                const categoryId = elm.dataset.category_id;
                 // console.log("categoryId = ", categoryId);
 
                 // Mettre à jour l'URL de l'API avec la nouvelle catégorie
@@ -28,22 +29,24 @@
 
     function parcourir_articles(apiUrl) {
         fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const destinationList = document.querySelector('.destination__list');
-            destinationList.innerHTML = ''; // Vider la liste avant d'ajouter les nouveaux articles
-            data.forEach(article => {
-                const articleElement = document.createElement('div');
+            .then(response => response.json())
+            .then(data => {
+                const destinationList = document.querySelector('.destination__list');
+                destinationList.innerHTML = ''; // Vider la liste avant d'ajouter les nouveaux articles
+                
+                data.forEach(article => {
+                    const articleElement = document.createElement('div');
+                    articleElement.classList.add
+                   
+                    articleElement.innerHTML = `
+                        <h3>${article.title.rendered}</h3>
 
-                articleElement.innerHTML = `
-                    <h3>${article.title.rendered}</h3>
-                    
-                    <p>${article.excerpt.rendered}</p>
-                    <a href="${article.link}">Lire plus</a>
-                `;
-                destinationList .appendChild(articleElement);
-            });
-        })
-        .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+                        <p>${article.excerpt.rendered}</p>
+                        <a href="${article.link}">Lire plus</a>
+                    `;
+                    destinationList .appendChild(articleElement);
+                });
+            })
+            .catch(error => console.error('Erreur lors de la récupération des articles:', error));
     }   
 })();
