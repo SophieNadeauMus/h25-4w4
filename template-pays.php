c2a7595d8f75c7838c5e52911ff30152a3447b49
Sophie Nadeau <sophienadeau.mus@outlook.com>
2025-05-23 10:40:33 -0400

    Ajout du contenu de l'introduction

--- template-pays.php
+++ template-pays.php
@@ -7,13 +7,22 @@
 <section class="pays">
   <div class="pays__intro">
     <div class="pays__intro__contenu">
-      <p><?php the_field('description_pays'); ?></p>
+      <h1><?php the_title(); ?></h1>
+      <p> <?php the_content(); ?></p>
     </div>
   </div>
+  <div class="pays__vague__1">
+    <?php genere_vague('rouge'); ?>
+  </div>
   <div class="pays__liste">
-    <?php pays_liste(); ?>
-    <h2 class="pays__liste__titre"></h2>
-    <div class="destination__list" data-method=search></div>
+    <div class="pays__liste__contenu">
+      <?php pays_liste(); ?>
+      <h2 class="pays__liste__titre"></h2>
+      <div class="destination__list" data-method=search></div>
+    </div>
+  </div>
+  <div class="pays__vague__2">
+    <?php genere_vague('rouge'); ?>
   </div>
 </section>
 <?php get_footer(); ?>
